7a4f4079261b78e4c8fb6204f668e5df21817bdd
mamaquig <mamaquig@student.42.fr>
2021-06-25 16:46:22 +0200

    env finished

--- srcs/requirements/wordpress/tools/init_wp.sh
+++ srcs/requirements/wordpress/tools/init_wp.sh
@@ -5,10 +5,10 @@ chmod +x /usr/local/bin/wp
 
 wp core download --path=/code --locale=fr_FR
 
-wp config create --dbname="newdb" --dbuser="mama" --dbpass="DBpassword" --dbhost="mariadb" --path="/code"
+wp config create --dbname="$MYSQL_NAME" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost="mariadb" --path="/code"
 
-wp core install --url="mamaquig.42.fr" --title="Inception" --admin_user="MamaLeBG" --admin_password="ILoveAntoine" --admin_email="xXx_m4m4l3_BG_du_27_xXx@live.fr" --path="/code"
+wp core install --url="mamaquig.42.fr" --title="Inception" --admin_user="$ADMIN_USER" --admin_password="$ADMIN_PASSWORD" --admin_email="$ADMIN_MAIL" --path="/code"
 
-wp user create membername member@email.com --role="author" --user_pass="memberpass" --path="/code"
+wp user create $USER_NAME $USER_MAIL --role="author" --user_pass="$USER_PASSWORD" --path="/code"
 
 exec "$@"
