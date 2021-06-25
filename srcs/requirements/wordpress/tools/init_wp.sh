45b3d8941e3cf487c4b9283620abe9f8fd31abc1
mamaquig <mamaquig@student.42.fr>
2021-06-25 21:45:22 +0200

    env fixed, password set for root

--- srcs/requirements/wordpress/tools/init_wp.sh
+++ srcs/requirements/wordpress/tools/init_wp.sh
@@ -1,11 +1,11 @@
-#! bin/sh
+#! /bin/sh
 
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -O /usr/local/bin/wp
 chmod +x /usr/local/bin/wp
 
 wp core download --path=/code --locale=fr_FR
 
-wp config create --dbname="$MYSQL_NAME" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost="mariadb" --path="/code"
+wp config create --dbname="$MYSQL_NAME" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost="mariadb" --path="/code" --dbprefix=wp_
 
 wp core install --url="mamaquig.42.fr" --title="Inception" --admin_user="$ADMIN_USER" --admin_password="$ADMIN_PASSWORD" --admin_email="$ADMIN_MAIL" --path="/code"
 
